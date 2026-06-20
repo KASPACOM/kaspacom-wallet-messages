1bd106f74ad52b93d6063306ec6b49d99decd995
Gilad Mucharer <giladm11@gmail.com>
2026-06-19 20:11:57 -0500

    adding sign inputs on sign pskt

--- dist/types/actions/requests/payloads/sign-pskt-transaction-action.interface.d.ts
+++ dist/types/actions/requests/payloads/sign-pskt-transaction-action.interface.d.ts
@@ -1,8 +1,21 @@
-import { PsktActionsEnum } from "../../../pkst-actions-enum.interface";
-import { ProtocolType } from "../../../protocol-type.enum";
+import { PsktActionsEnum } from '../../../pkst-actions-enum.interface';
+import { ProtocolType } from '../../../protocol-type.enum';
+export declare enum WalletPsktSighashTypeEnum {
+    All = 0,
+    None = 1,
+    Single = 2,
+    AllAnyOneCanPay = 3,
+    NoneAnyOneCanPay = 4,
+    SingleAnyOneCanPay = 5
+}
+export interface WalletPsktSignInput {
+    index: number;
+    sighashType?: WalletPsktSighashTypeEnum;
+}
 export interface SignPsktTransactionActionInterface {
     psktTransactionJson: string;
     signOnly?: boolean;
+    signInputs?: WalletPsktSignInput[];
     submitTransaction?: boolean;
     protocol?: ProtocolType | string;
     protocolAction?: PsktActionsEnum | string;
--- dist/types/actions/requests/payloads/sign-pskt-transaction-action.interface.js
+++ dist/types/actions/requests/payloads/sign-pskt-transaction-action.interface.js
@@ -1,1 +1,9 @@
-export {};
+export var WalletPsktSighashTypeEnum;
+(function (WalletPsktSighashTypeEnum) {
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["All"] = 0] = "All";
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["None"] = 1] = "None";
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["Single"] = 2] = "Single";
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["AllAnyOneCanPay"] = 3] = "AllAnyOneCanPay";
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["NoneAnyOneCanPay"] = 4] = "NoneAnyOneCanPay";
+    WalletPsktSighashTypeEnum[WalletPsktSighashTypeEnum["SingleAnyOneCanPay"] = 5] = "SingleAnyOneCanPay";
+})(WalletPsktSighashTypeEnum || (WalletPsktSighashTypeEnum = {}));
